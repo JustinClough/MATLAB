6b335f2304866a3adfed2f421ea3a6763bd5a739
JustinClough <clougj@rpi.edu>
2017-01-30 18:16:27 -0500

    Work on HW 2

--- Graduate_Courses/CSCI4800/HomeWork/Set2/CSCI4800HW2.m
+++ Graduate_Courses/CSCI4800/HomeWork/Set2/CSCI4800HW2.m
@@ -8,32 +8,100 @@
 clear
 home
 close all
-DIR = 'Graduate_Courses/CSCI4800/HomeWork/Set2';
-fileID = fopen( [DIR 'CSCI4800HW2Output1.txt'], 'w');
+DIR = 'Graduate_Courses/CSCI4800/HomeWork/Set2/';
 
 %% Problem 1
+%open file to write results to
+fileID = fopen( [DIR 'CSCI4800HW2Output1.txt'], 'w');
+fprintf(fileID, 'Solutions to Problem 1: \r\n');
+
+%Part A:
+
+%Assign variables
+xmin1 = -2;
+xmax1 = 2;
+dx1 = 0.1;
+
+%Evaluate function over range
+for i=1:(xmax1-xmin1)/dx1+1
+    x1(i) = xmin1+(i-1)*dx1;
+    f1(i) = x1(i)^2+x1(i)-cos(x1(i));
+end
+clear i
+
+%Create plot image
+figure
+hold on
+plot(x1,f1)
+grid on
+title('Problem 1: Value of f(x)');
+xlabel('x values');
+ylabel('Function values');
+
+%save image as .jpg file
+print( [DIR 'CSCI4800HW2plot1'], '-djpeg');
+
+%Part B:
+
+%Estimate minimum number of iterations to solve to P digits of accuracy
+P = 10;
+n1 = P*log2(10)+log2(1/2);
+n1 = ceil(n1); %round up since fractional iterations dont exist
+n = n1; %same accuracy over same bounds means same number of iterations
+
+fprintf(fileID,...
+    'Minimum number of iterations for %3.0f digits of accuracy: %3.0f\r\n' ...
+    ,P, n);
+
+
+%close results file
+fclose(fileID);
 
 
 
 %% Problem 2
 fprintf(fileID, 'Results for Problem 2 (not including plot):\r\n');
 
+%declare function
+f2=@(x) (exp(x-2)+x^3-x);
+
+xmin2 = -2;
+xmax2 = 2;
+dx2 = 0.1;
+
+for i = 1:(xmax2-xmax2)/dx+1;
+    x2(i) = xmin2 + (i-1)*dx;
+    func2(i) = f(x(i));
+end
+clear i
+
+%Create plot image
+figure
+hold on
+plot(x2, func2)
+grid on
+title('Problem 2: Value of f(x)');
+xlabel('x values');
+ylabel('Function values');
+
+%save image as .jpg file
+print( [DIR 'CSCI4800HW2plot2'], '-djpeg');
 
 
 
-%Close CSCI4800HW1Output1.txt
-fclose(fileID);
-
-%% Problem 3
-
-%Open new text file to write to
-fileID = fopen( [DIR 'CSCI4800HW1Output2.txt'], 'w');
-fprintf(fileID, 'Results for Problem 3:\r\n');
 
 
 
-
-
-%Close file:
-fclose(fileID);
+% %% Problem 3
+%
+% %Open new text file to write to
+% fileID = fopen( [DIR 'CSCI4800HW1Output2.txt'], 'w');
+% fprintf(fileID, 'Results for Problem 3:\r\n');
+%
+%
+%
+%
+%
+% %Close file:
+% fclose(fileID);
 
---
+++ Graduate_Courses/CSCI4800/HomeWork/Set2/bisect.m
@@ -0,0 +1,57 @@
+% bisect : Find an approximate soluiton to f(x) = 0 using bisection
+%
+% f (input) : function f(x)
+% a,b (inputs) : interval to search that brackets the root, a<b and
+% f(a)*f(b) <= 0 
+% tol (input) : tolerance
+% maxInterations (input): maximum number of iterations allowed
+% xc (output) : approximate solution
+
+function xc = bisect( f,a,b,tol, maxIterations)
+
+%Catch user mistakes
+if sign(f(a))*sign(f(b))>=0
+    error('f(a)f(b)<0 not satisfied!\n')
+elseif a > b
+    error('a<b not satisfied!\n')
+end
+
+fa = f(a);
+fb = f(b);
+%assign iteration counter
+k = 0;
+
+while abs((b-a)/2)>tol
+    k = k + 1;
+    c = (a+b)/2;
+    fc = f(c);
+    
+    %Report satus to user
+    fprintf(...
+        'bisect: k=%2.0d, a=%11.4e f(a)=%8.2e b=%11.4e f(b)=%8.2e c=%14.7e\n'...
+        ,        k,       a,       fa,        b,       fb,        c);
+    %compare midpoint to solution and edges
+    if fc ==0
+        %Solution found. 
+        break;
+    elseif sign(fc)*sign(fa)<0
+        %replace right bracket
+        b = c;
+        fb = fc;
+    else
+        %replace left bracket
+        a = c;
+        fa = fc;
+    end
+    
+    %compare iteration counter to maximum iterations
+    if k >= maxIterations
+        error( ...
+            'Maximum number of iterations (%11.4f) reached.\n'...
+            , maxIterations)
+    end
+        
+end
+
+%Report result
+xc = (a+b)/2;
