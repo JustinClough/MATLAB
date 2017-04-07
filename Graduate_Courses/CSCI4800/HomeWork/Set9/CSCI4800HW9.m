a2a05c8fa61b7778aa60cf6ca123d8004de37f14
JustinClough <clougj@rpi.edu>
2017-04-07 09:22:33 -0400

    Finishes CSCI4800HW9 Problem 1.c

--- Graduate_Courses/CSCI4800/HomeWork/Set9/CSCI4800HW9.m
+++ Graduate_Courses/CSCI4800/HomeWork/Set9/CSCI4800HW9.m
@@ -9,14 +9,51 @@ home
 close all
 DIR = 'Graduate_Courses/CSCI4800/HomeWork/Set9/';
 
-% print( [DIR 'CSCI4800HW9<plotName>'], '-djpeg');
 
 % fileID = fopen( [DIR 'CSCI4800HW8Output1c1.txt'], 'w');
 %% Problem 1
 
+% Define local functions
+f_1 = @(x) (exp(x^2));
+fx_1 = @(x) (2*x*exp(x^2));
 
-% Close file
-fclose(fileID);
+% Compute Exact Derivative
+x = 1;
+Exact = fx_1(x);
+
+for i = 1:40
+  % Set h
+  h = 2^(-i);
+
+  % Compute Approximations to derivative
+  d_2(i) = (f_1(x+h)-f_1(x-h))/(2*h);
+  d_4(i) = (f_1(x-2*h)-8*f_1(x-h)+8*f_1(x+h)-f_1(x+2*h))/(12*h);
+
+  % Compute Error in Approximation
+  e_2(i) = abs(d_2(i) - Exact);
+  e_4(i) = abs(d_4(i) - Exact);
+
+  % store h value for plotting
+  H(i) = h;
+end
+
+% Get machine accuracy
+E_m = eps;
+H_opt = (E_m)^(1/3);
+
+
+% Plot Error 
+figure
+loglog(H,e_2, '-k');
+hold on
+loglog(H,e_4, '--k');
+loglog([H_opt H_opt],[10^(-15) 1],':k');
+legend('Second Order Approx.','Fourth Order Approx.','Optimal Step Line','Location','SouthWest');
+title('Problem 1.c: Approximation Errors');
+xlabel('Log(Step Size)');
+ylabel('Log(Error)');
+print( [DIR 'CSCI4800HW9plot1c'], '-djpeg');
+
 
 %% Problem 2
 
