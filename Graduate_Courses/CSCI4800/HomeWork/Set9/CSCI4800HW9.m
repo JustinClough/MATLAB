5851bbdee7c65cfc58e5fb585294a9c7d130dd4a
JustinClough <clougj@rpi.edu>
2017-04-07 08:12:34 -0400

    Begins CSCI4800 HW 9

---
+++ Graduate_Courses/CSCI4800/HomeWork/Set9/CSCI4800HW9.m
@@ -0,0 +1,29 @@
+%Written for: Computes and outputs Homework set 9 solutions for CSCI 4800-2
+%Written by: Justin Clough
+%Written on: 04/13/2017
+
+%% Prep workspace
+
+clear
+home
+close all
+DIR = 'Graduate_Courses/CSCI4800/HomeWork/Set9/';
+
+% print( [DIR 'CSCI4800HW9<plotName>'], '-djpeg');
+
+% fileID = fopen( [DIR 'CSCI4800HW8Output1c1.txt'], 'w');
+%% Problem 1
+
+
+% Close file
+fclose(fileID);
+
+%% Problem 2
+
+
+%% Problem 3
+
+
+
+%% Problem 4
+
