b347234ea69c40d6e56c20a7d202f149c991ac0d
JustinClough <clougj@rpi.edu>
2017-04-26 20:17:30 -0400

    Does Problem 10

--- Graduate_Courses/CSCI4800/HomeWork/Set10/CSCI4800HW10.m
+++ Graduate_Courses/CSCI4800/HomeWork/Set10/CSCI4800HW10.m
@@ -17,16 +17,12 @@ func=@(w,t) (-w+t);
 h=1/4;
 t0 = 0;
 w(1) = 1;
-for i=1:4
-    j = i-1;
+for i=2:5
+    i
+    j = i-1
     t(i) = t0+j*h;
-    if j<1
-        w(i) = w0+h*func(w0,t(i));
-    else
-        w(i) = w(j)+h*func(w(j),t(i));
-    end    
+    w(i) = w(j)+h*func(w(j),t(j));
 end
-t(i+1) = t0+(i+1)*h;
 t
 w
 plot(t,w)
