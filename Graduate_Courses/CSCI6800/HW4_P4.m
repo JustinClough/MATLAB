d1548bb7f7936da78706dd0523e91dbc3f47cc96
Justin Clough <clougj@rpi.edu>
2017-10-02 18:25:15 -0400

    CSCI 6800: Adds dir, begins HW4P4

---
+++ Graduate_Courses/CSCI6800/HW4_P4.m
@@ -0,0 +1,12 @@
+% Written by: Justin Clough
+% Written on: 10/02/2017
+% Written for:
+%   Prepares workspaces and then executes 
+%   needed commands to complete P4 of HW 4
+%   for CSCI6800
+
+% Prep workspace
+clear
+hold off
+
+
