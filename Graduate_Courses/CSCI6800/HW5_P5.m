374a831d2d9105fcac1b30b7d8ee17cacdf4e699
Justin Clough <clougj@rpi.edu>
2017-10-16 18:34:05 -0400

    Part b for problem 5

--- Graduate_Courses/CSCI6800/HW5_P5.m
+++ Graduate_Courses/CSCI6800/HW5_P5.m
@@ -42,11 +42,51 @@ fprintf( fileID, '|| Q^(T)Q - I ||_2 = %2f \r\n', norm( Q'*Q - eye(m), 2));
 fprintf( fileID, '|| Qh^(T)Qh - I ||_2 = %2f \r\n', norm( Qh'*Qh - eye(m), 2));
 fprintf( fileID, '\r\n');
 
+clear W Rh Q R Qh;
+
 %% Part b)
 fprintf( fileID, 'Results from Part b)\r\n');
 
+Z = [ 1, 2, 3;
+      4, 5, 6;
+      7, 8, 7;
+      4, 2, 3;
+      4, 2, 2;];
 
+[Qm, Rm] = mgs( Z);
+[W, Rh] = house( Z);
+Qh = formQ( W);
+[Q, R] = qr( Z, 0);
 
+% Adjust qr results such that R_ii comp.s are positive
+D = diag(sign(diag(R)));
+Q = Q*D;
+R = D*R;
+
+% Adjust house, formQ results such that R_ii comp.s are positive
+Dh = diag(sign(diag(Rh)));
+Qh = Qh*Dh;
+Rh = Dh*Rh;
+
+fprintf( fileID, '|| Z - QR ||_2 = %2f \r\n', norm( Z - Q * R, 2));
+fprintf( fileID, '|| Z - QhRh ||_2 = %2f \r\n', norm( Z - Qh * Rh, 2));
+fprintf( fileID, '|| Z - QmRm ||_2 = %2f \r\n', norm( Z - Qm * Rm, 2));
+
+fprintf( fileID, '|| Q^(T)Q - I ||_2 = %2f \r\n', ...
+    norm( Q'*Q - eye( size( Q'* Q)), 2));
+fprintf( fileID, '|| Qh^(T)Qh - I ||_2 = %2f \r\n', ...
+    norm( Qh'*Qh - eye(size( Qh'*Qh)), 2));
+fprintf( fileID, '|| Qm^(T)Qm - I ||_2 = %2f \r\n', ...
+    norm( Qm'*Qm - eye(size( Qm'*Qm)), 2));
+
+fprintf( fileID, '|| Qm - Q ||_2 = %2f \r\n', norm( Qm - Q, 2));
+fprintf( fileID, '|| Rm - R ||_2 = %2f \r\n', norm( Rm - R, 2));
+
+fprintf( fileID, '|| Qh - Q ||_2 = %2f \r\n', norm( Qh - Q, 2));
+fprintf( fileID, '|| Rh - R ||_2 = %2f \r\n', norm( Rh - R, 2));
+
+fprintf( fileID, '|| Qh - Qm ||_2 = %2f \r\n', norm( Qh - Qm, 2));
+fprintf( fileID, '|| Rh - Rm ||_2 = %2f \r\n', norm( Rh - Rm, 2));
 
 %% Clean up
 fclose( fileID);
