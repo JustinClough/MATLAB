21e786077c5faf6a967eec9ef4effab6122dcba0
Justin Clough <clougj@rpi.edu>
2017-10-16 12:10:13 -0400

    Adds an outline for house.m

---
+++ Graduate_Courses/CSCI6800/house.m
@@ -0,0 +1,14 @@
+%% Function house
+% Computes implicit representation of a
+% QR factorization using Householder
+% reflector.
+%% Inputs:
+% A The matrix to factor
+%% Outputs:
+% W Lower triangular matrix, columns are
+%   the Householder reflector vectors.
+% R The upper triangular matrix.
+
+[W, R] = house( A)
+
+end
