e67c1ced93632dadbe4470539cc9e1cc1d38b2e8
JustinClough <clougj@rpi.edu>
2017-10-16 19:44:01 -0400

    Adjusts spacing

--- Graduate_Courses/CSCI6800/printNicelyP6.m
+++ Graduate_Courses/CSCI6800/printNicelyP6.m
@@ -1,6 +1,7 @@
 %% Function printNicelyP6
 % Prints as defined for HW5P6.
 %% Inputs:
+% fileID The file to print to
 % xa Normal Direct Solve
 % xb CLGS
 % xc MGS
@@ -11,7 +12,19 @@
 %% Outputs:
 % e An error indicator.
 
+function e = printNicelyP6( fileID, xa, xb ,xc ,xd ,xe ,xf ,xg)
+e = 0;
+n = size( xa);
 
-function e = printNicelyp6( xa, xb ,xc ,xd ,xe ,xf ,xg)
+fprintf( fileID, ' Normal                 CLGS                   MGS                                    HOUSE\n');
+for i=1:n
+  fprintf(fileID, '%22.15e %22.15e %22.15e %22.15e\n',xa(i),xb(i),xc(i),xd(i));
+end;
+
+fprintf(fileID, '  Matlab_QR              backslash              SVD\n');
+
+for i=1:n
+  fprintf(fileID, ' %22.15e %22.15e %22.15e\n',xe(i),xf(i),xg(i));
+end;
 
 end
