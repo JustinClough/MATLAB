b6d91da1127f895342f360c517fa21ceafa1ef98
JustinClough <clougj@rpi.edu>
2017-10-16 18:55:01 -0400

    Outlines printNicelyp6.m

---
+++ Graduate_Courses/CSCI6800/printNicelyP6.m
@@ -0,0 +1,17 @@
+%% Function printNicelyP6
+% Prints as defined for HW5P6.
+%% Inputs:
+% xa Normal Direct Solve
+% xb CLGS
+% xc MGS
+% xd House
+% xe Matlab QR
+% xf Backslash
+% xg SVD
+%% Outputs:
+% e An error indicator.
+
+
+function e = printNicelyp6( xa, xb ,xc ,xd ,xe ,xf ,xg)
+
+end
