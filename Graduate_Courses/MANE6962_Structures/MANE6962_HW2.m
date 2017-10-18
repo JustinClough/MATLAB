e4559d3d3cdeae85ea4d6d7c5502b0d94e7eee7d
JustinClough <clougj@rpi.edu>
2017-10-18 15:41:17 -0400

    Adds precalc'd values; compliance matrix

--- Graduate_Courses/MANE6962_Structures/MANE6962_HW2.m
+++ Graduate_Courses/MANE6962_Structures/MANE6962_HW2.m
@@ -30,12 +30,53 @@ rho = 2.78 * (100)^(3) / 1000;
 % Number of beam segments 
 n = 5;
 
+%% Precaculate Utility Values
+% Area moment of interia (meters)^4
+AMI = w * t^(3) / 12;
+
+% Array of beam segment center locations
+x = zeros( 1, n);
+for i = 1:n
+  x(i) = (i - 1) * (L/n) + (L / (2 * n) );
+end
+clear i;
+
+
 %% Populate Mass Matrix
 % Lumped mass model used here
 
+% Components have units of Kilograms
 Mass = zeros( n, n);
 for i = 1:n
-  Mass( i, i) = L * rho * w * t / n;
+  Mass(i, i) = L * rho * w * t / n;
 end
+clear i;
 
 %% Populate Stiffness Matrix
+% First construct compliance matrix
+
+% Components have units of Meters/Newton
+S = zeros( n, n);
+for i = 1:n
+  for j = 1:n
+    i 
+    j
+    if ( i <= j)
+      Macaulay = 0;
+    else
+      Macaulay = x(i) - x(j);
+    end
+    tmp = Macaulay^(3) - (x(i))^(3) + 3 * x(j) * (x(i))^(2);
+    % Scale by Effective rigidity
+    S(i,j) = (1/ (6 * E * AMI)) * tmp;
+    tmp
+    S
+    pause
+  end
+end
+
+
+
+
+
+
