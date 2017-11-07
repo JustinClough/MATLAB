6cf1fb317c8d31af669efe5f20371d45f25c1d41
JustinClough <clougj@rpi.edu>
2017-11-07 11:47:58 -0500

    CSCI6800: Adds forward-elim as function

---
+++ Graduate_Courses/CSCI6800/forwardElimination.m
@@ -0,0 +1,27 @@
+%% Function forwardElimnation
+% Finds the solution to y = Lb where L
+% is a lower diagonal matrix.
+%% Inputs:
+% L The lower diagonal matrix.
+% b The forcing vector.
+%% Outputs:
+% y The solution vector.
+
+function y = forwardElimination( L, b)
+
+[m, n] = size( L);
+if (m ~= n)
+  error('Passed matrix not square. \n');
+end % if
+
+y = zeros( m, 1);
+
+for i = 1:n
+    tmp = b(i);
+    for j=1:i-1
+        tmp = tmp - L(i,j)*y(j);
+    end
+    y(i) = tmp/L(i,i);
+end
+
+end
