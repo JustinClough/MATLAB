3a44025be82227930c1b6a0c562d16ef054d2fd7
JustinClough <clougj@rpi.edu>
2017-11-07 11:47:36 -0500

    CSCI6800: Adds back-sub as function

---
+++ Graduate_Courses/CSCI6800/backwardSubstitution.m
@@ -0,0 +1,27 @@
+%% Function forwardElimnation
+% Finds the solution to x = Uy where U
+% is a upper diagonal matrix.
+%% Inputs:
+% U The upper diagonal matrix.
+% y The forcing vector.
+%% Outputs:
+% x The solution vector.
+
+function x = backwardSubstitution( U, y)
+
+[m, n] = size( U);
+if (m ~= n)
+  error('Passed matrix not square. \n');
+end % if
+
+x = zeros( m, 1);
+x(n) = (y(n))/(U(n,n));
+
+for I = 1:n
+    i = n-I+1;
+    tmp = y(i);
+    for j = i+1:n
+        tmp = tmp - U(i,j)*x(j);
+    end
+    x(i) = tmp/U(i,i);
+end
