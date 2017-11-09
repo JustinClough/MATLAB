2f8d6f7ceb2f461450d89525e7b5efe645e11bb1
JustinClough <clougj@rpi.edu>
2017-11-09 12:39:58 -0500

    Adds printing results to file

--- Graduate_Courses/CSCI6800/HW8_P4.m
+++ Graduate_Courses/CSCI6800/HW8_P4.m
@@ -7,6 +7,8 @@
 
 % Prep Workspace
 clear
+DIR = 'Graduate_Courses/CSCI6800/' ;
+fileID = fopen( [DIR 'CSCI6800HW8P4.txt'], 'w');
 
 % Define constants
 
@@ -24,16 +26,32 @@ b = [ 7;
 % Get L, U, and P factors of A
 [ L, U, P] = lufactor( A);
 
+% Print P, L, and U to file
+fprintf( fileID, 'P = \r\n');
+fprintf( fileID, [repmat('%f\t', 1, size(P,2)) '\n'], P);
+
+fprintf( fileID, 'L = \r\n');
+fprintf( fileID, [repmat('%f\t', 1, size(L,2)) '\n'], L);
+
+fprintf( fileID, 'U = \r\n');
+fprintf( fileID, [repmat('%f\t', 1, size(U,2)) '\n'], U);
+
 % Measure if PA = LU
-fprintf( 'Norm_2 of PA-LU: \n');
-norm( (P * A - L * U), 2)
+n1 = norm( (P * A - L * U), 2); 
+fprintf( fileID, 'Norm_2 of PA-LU = %f \n', n1);
 
 %% Part b)
 % Solve system
 x =  lusolve( b, L, U, P);
 
+fprintf( fileID, 'x = \r\n');
+fprintf( fileID, [repmat('%f\t', 1, size(x,2)) '\n'], x);
+
 % Check forward solution
 b_f = A * x;
 
-fprintf( 'Norm_2 of b_f - b: \n' );
-norm( (b_f - b), 2)
+n2 = norm( (b_f - b), 2);
+fprintf( fileID, 'Norm_2 of b_f - b = %f \n', n2 );
+
+%% Clean up
+fclose( fileID);
