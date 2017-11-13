a54716e16b710ded7d35a0f10f89489714c471d6
JustinClough <clougj@rpi.edu>
2017-11-13 12:31:55 -0500

    Adds jacobi_*.m functions, outlines

---
+++ Graduate_Courses/CSCI6800/jacobi_error.m
@@ -0,0 +1,23 @@
+%% Function jacobi_error
+% Calculates the L2 error at each Jacobi iterations 
+% when solving the problem Ax=b for HW9_P4.
+%% Inputs:
+% A The stiffness matrix.
+% b The forcing vector.
+% tol The solution tolerance.
+%% Outpus:
+% E The error at each iteration.
+
+function E = jacobi_error( A, b, tol)
+
+[m, n] = size( A);
+f = zeros( m, 1);
+
+iter = 1;
+E(1) = tol * 2;
+while E(iter) > tol
+  f = jacobi_step( f, A, b);
+  E(iter) = p5_get_error( f);
+end % while
+
+end
---
+++ Graduate_Courses/CSCI6800/jacobi_step.m
@@ -0,0 +1,13 @@
+%% Function jacobi_step
+% Performs a single Jacobi step on Ax=b
+%% Inputs:
+% old The solution at the previous step.
+% A   The stiffness matrix.
+% b   The forcing vector.
+%% Outputs:
+% sol The solution at the new step.
+
+function sol = jacobi_step( old, A, b)
+
+
+end
