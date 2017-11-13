6551511b15cccde2c83f440f7e2bc51b254f6faa
JustinClough <clougj@rpi.edu>
2017-11-13 17:53:35 -0500

    Fixes off-by-one error, BC's

--- Graduate_Courses/CSCI6800/build_hw8_p5.m
+++ Graduate_Courses/CSCI6800/build_hw8_p5.m
@@ -9,19 +9,27 @@
 
 function [A, x, b] = build_hw8_p5( m)
 
-A = 2*eye( m+1, m+1)  ...
-  + diag( (-1) * ones( 1, m),  1)  ...
-  + diag( (-1) * ones( 1, m), -1);
+A = 2*eye( m, m)  ...
+  + diag( (-1) * ones( 1, m-1),  1)  ...
+  + diag( (-1) * ones( 1, m-1), -1);
 
-x = zeros( m+1, 1);
+x = zeros( m+2, 1);
 dx = pi / (m+1);
-for i = 1:(m+1)
-  x( i) = i * dx;
+for i = 1:(m+2)
+  x( i) = (i-1) * dx;
 end
 
-b            = zeros( m+1, 1);
-b            = (dx^2) * cos( x);
-b(1)         = b(1) + 1;
+b = zeros( m, 1);
+j = 0;
+for i = 1:m+2
+  if (i~=1) && (i~=m+2)
+    j = j+1;
+    b(j) = (dx^2) * cos( x(i));
+  end
+end
+
+% Apply BCs
+b(1) = b(1) + 1;
 b( length( b)) = b( length(b)) - 1;
 
 end
