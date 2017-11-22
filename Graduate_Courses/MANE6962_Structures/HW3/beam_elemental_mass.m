a2af210e9a9766b4c550b1ace52e4476e17a02d0
JustinClough <clougj@rpi.edu>
2017-11-22 14:18:30 -0500

    Structures: Allows variable thickness of beam...  in the elemental mass function

--- Graduate_Courses/MANE6962_Structures/HW3/beam_elemental_mass.m
+++ Graduate_Courses/MANE6962_Structures/HW3/beam_elemental_mass.m
@@ -1,16 +1,23 @@
 %% Function beam_elemental_mass
 % Creates the elemental mass matrix for cubic Hermetian
-% beam elements. 
+% beam elements. The geometric mean is not taken.
 %% Inputs:
 % I The element number.
 % n_el Total number of elements.
 % wb The base width.
 % wt The tip width.
+% tb The base thickness of the beam
+% tt The tip thickness of the beam.
 % L  The length of the beam.
-% ma The mass per unit area (area density).
+% rho The density of the beam.
 %% Outputs
+% M The elemental mass matrix.
 
-function M = beam_elemental_mass( I, n_el, wb, wt, L, ma)
+function M = beam_elemental_mass( I, n_el, wb, wt, tb, tt, L, rho)
+
+% Number of Degrees of Freedom per element
+n_dof = 4;
+M = zeros( n_dof, n_dof);
 
 
 end
