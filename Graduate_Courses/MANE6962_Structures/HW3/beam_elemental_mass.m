3accf4ced02420f548109ae989011506b73e0afa
JustinClough <clougj@rpi.edu>
2017-11-22 07:46:00 -0500

    Structures: outline for elemental mass function

---
+++ Graduate_Courses/MANE6962_Structures/HW3/beam_elemental_mass.m
@@ -0,0 +1,16 @@
+%% Function beam_elemental_mass
+% Creates the elemental mass matrix for cubic Hermetian
+% beam elements. 
+%% Inputs:
+% I The element number.
+% n_el Total number of elements.
+% wb The base width.
+% wt The tip width.
+% L  The length of the beam.
+% ma The mass per unit area (area density).
+%% Outputs
+
+function M = beam_elemental_mass( I, n_el, wb, wt, L, ma)
+
+
+end
