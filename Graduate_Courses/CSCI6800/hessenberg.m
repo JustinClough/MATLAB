e9ee31b4aa1e134dbd00bc6b236ca2e5c9b318a0
JustinClough <clougj@rpi.edu>
2017-12-06 14:44:09 -0500

    CSCI6800: Adds function body to hessenberg

--- Graduate_Courses/CSCI6800/hessenberg.m
+++ Graduate_Courses/CSCI6800/hessenberg.m
@@ -14,4 +14,13 @@ if (m ~=n)
   error( 'Passed Matrix A not square');
 end
 
+for k = 1: (m-2)
+  x = A( k+1:m, k);
+  e = zeros( size(x));
+  e(1) = 1;
+  v = sign( x(1)) * norm( x, 2) * e + x;A
+  v = v / norm( v, 2);
+
+end
+
 end
