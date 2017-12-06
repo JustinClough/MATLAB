3e9d5c0a3139bce9b3fc1ad42c4209eb7141f552
JustinClough <clougj@rpi.edu>
2017-12-06 18:49:51 -0500

    CSCI6800: Outlines formQh function

---
+++ Graduate_Courses/CSCI6800/formQh.m
@@ -0,0 +1,12 @@
+%% Function formQh
+% Takes the Householder vector collection W from 
+% the hessenberg function and forms Q.
+%% Inputs:
+% W The matrix to transform.
+%% Outputs:
+% Q The rotation matrix.
+
+function Q = formQh( W)
+
+
+end
