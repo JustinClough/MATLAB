ec6e428c9c265d7fddf05053e92fe219aa6b069e
JustinClough <clougj@rpi.edu>
2017-12-06 18:45:19 -0500

    CSCI6800: Adds W matrix to hessenberg

--- Graduate_Courses/CSCI6800/hessenberg.m
+++ Graduate_Courses/CSCI6800/hessenberg.m
@@ -14,12 +14,16 @@ if (m ~=n)
   error( 'Passed Matrix A not square');
 end
 
+W = zeros( m, m);
+
 for k = 1: (m-2)
   x = A( k+1:m, k);
   e = zeros( size(x));
   e(1) = 1;
   v = sign( x(1)) * norm( x, 2) * e + x;A
   v = v / norm( v, 2);
+
+  W(:,k) = v;
 
   A( k+1:m, k:m) = A( k+1:m, k:m) - 2 * v * (v' * A( k+1:m, k:m));
   A( 1:m, k+1:m) = A( 1:m, k+1:m) - 2 * ( A( 1:m, k+1:m) * v) * v';
