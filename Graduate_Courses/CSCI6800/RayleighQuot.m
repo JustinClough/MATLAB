77adbf0a8b48eb6a35ce2a65417fe3ea161020c5
JustinClough <clougj@rpi.edu>
2017-12-06 20:02:36 -0500

    CSCI6800: Adds outline of RayleighQuot function

---
+++ Graduate_Courses/CSCI6800/RayleighQuot.m
@@ -0,0 +1,14 @@
+%% Function RayleighQuot
+% Estimates the nearest eigenvalue of a given matrix
+% based on an intial guess to a specified tolerance.
+%% Inputs:
+% A   The matrix to evaluate.
+% mu  The initial guess.
+% tol The solution tolerance.
+%% Outputs:
+% lambda The estimated eigenvalue.
+
+function lambda = RayleighQuot( A, mu, tol)
+
+
+end
