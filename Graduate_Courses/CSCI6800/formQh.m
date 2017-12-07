109254c9785e6ebde58b2ee4c63170587d7bc563
JustinClough <clougj@rpi.edu>
2017-12-06 20:00:38 -0500

    CSCI6800: Adds body to formQh function

--- Graduate_Courses/CSCI6800/formQh.m
+++ Graduate_Courses/CSCI6800/formQh.m
@@ -8,5 +8,18 @@
 
 function Q = formQh( W)
 
+[m, n] = size( W);
+Q = eye( m, n);
+
+for j = 1:n
+  k = 1 + (n - j);
+
+  v = W(:, k);
+
+  Q(:,k:m) = Q(:, k:m) - 2 * v * (v' * Q(:, k:m));
+
+end
+
+  
 
 end
