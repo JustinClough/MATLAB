f6564ed97c7d0d093d080cdd948143a139cc3a9d
JustinClough <clougj@rpi.edu>
2018-02-16 13:49:19 -0500

    res: Uses two reference points for calibration

--- research/creep_mat_prop.m
+++ research/creep_mat_prop.m
@@ -7,14 +7,23 @@ home;
 
 % Adjust these parameters accordingly
 csv_fname = 'stress_strain_points.csv';
-x_units   = 'Percent Strain';
+x_units   = 'Nominal Strain';
 y_units   = 'MPa';
 knee_pt   = 60;
 
-x_refPoint = 40; 
+x_refPoint1 = 0.20; 
+           % 20 percent strain
+x_refPoint2 = 0.40; 
            % 40 percent strain
-y_refPoint = 1400;
+
+y_refPoint1 = 600;
+           % 600 MPa
+y_refPoint2 = 1400;
            % 1400 MPa
+
+% The actual data starts at point 6 and after
+data_start = 6;
+
 
 %% Read in csv
 Data = csvread( csv_fname);
@@ -36,7 +45,7 @@ for i = 1:n
   y_raw(i) = y_raw(i) - y_o
 end
 
-% Rotate to be in first quadrant
+% Flip to be in first quadrant
 if mean(x_raw) < 0
   x_raw = -x_raw;
 end
@@ -44,21 +53,19 @@ if mean(y_raw) < 0
   y_raw = -y_raw;
 end
 
-figure
-plot(x_raw, y_raw, '*')
-hold on
-title('Translated and Rotated Imported Data')
-xlabel('X Position [px]')
-ylabel('Y Position [px]')
-
 % Scale to appropriate units
-x_scale = (x_raw(2) - x_raw(1)) / x_refPoint;
-        % Units of pixels per x_units
-y_scale = (y_raw(3) - y_raw(1)) / y_refPoint;
-        % Units of piyels per y_units
+x_scale1 = (x_raw(2) - x_raw(1)) / x_refPoint1;
+x_scale2 = (x_raw(3) - x_raw(1)) / x_refPoint2;
+x_scale  = mean( [x_scale1 x_scale2]);
+% Units of pixels per x_units
 
-x = x_raw( 4:n) / x_scale;
-y = y_raw( 4:n) / y_scale;
+y_scale1 = (y_raw(4) - y_raw(1)) / y_refPoint1;
+y_scale2 = (y_raw(5) - y_raw(1)) / y_refPoint2;
+y_scale  = mean( [y_scale1 y_scale2]);
+% Units of pixels per y_units
+
+x = x_raw( data_start:n) / x_scale;
+y = y_raw( data_start:n) / y_scale;
 
 figure
 plot(x, y, '*')
@@ -68,16 +75,25 @@ xlabel( ['X Data [' x_units ']']);
 ylabel( ['Y Data [' y_units ']']);
 
 %% Estimate Material Properties
-x1 = x(4:knee_pt);
-y1 = y(4:knee_pt);
+x1 = x( 1:(knee_pt - data_start + 1));
+y1 = y( 1:(knee_pt - data_start + 1));
 
 % Not strictly enforcing an origin intercept
-E = [ones( length(x1), 1) x1] \y1;
+sol = [ones( length(x1), 1) x1] \y1;
+E = sol( 2);
 
 % Adjust for percent strain to nominal 
-E = E *100;
-fprintf(['Youngs Modulus, E = '])
+fprintf(['Youngs Modulus, '])
 E
 fprintf([ y_units '\n'])
+
+testx = min(x1):( x1( length(x1)) / 100):x1( length(x1));
+testy = E * testx + sol(1);
+
+hold on
+plot( testx, testy)
+grid on
+legend( 'Data', 'Fitted Line', 'Location', 'southeast' )
+
 
 
